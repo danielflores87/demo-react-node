b5accb9a9fb9063b1667c9c72276c2c8c2784a6e
danielflores <daniel.flores.87@hotmail.com>
2024-10-20 18:23:22 -0500

    ajuste archivos

--- demo-api/src/server.js
+++ demo-api/src/server.js
@@ -4,18 +4,18 @@ const cors = require("cors");
 const routes = require("./routes");
 
 const app = express();
-const port = 4200;
+const port = 3000;
 
 
 app.use(express.json());
 app.use(cors());
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//   })
+// );
 
 app.use("/api", routes);
 
