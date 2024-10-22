a475cd48609289f73218cc12819f531534164a20
Daniel Flores <83089838+danielflores87@users.noreply.github.com>
2024-10-22 09:28:29 -0500

    Update eslint.config.mjs

--- demo-api/eslint.config.mjs
+++ demo-api/eslint.config.mjs
@@ -15,12 +15,6 @@ export default [
   pluginJs.configs.recommended,
   {
     rules: {
-      // // Aquí puedes personalizar las reglas según tus necesidades
-      // "no-console": "off", // Permite el uso de console.log (útil para depuración en desarrollo)
-      // "prefer-const": "warn", // Recomienda usar const en lugar de let cuando sea posible
-      // "no-unused-vars": "warn", // Muestra advertencias para variables no utilizadas
-      // "consistent-return": "error", // Asegura que todas las rutas de retorno en funciones devuelvan un valor
-      // // Agrega más reglas según tus preferencias y necesidades
     },
   },
 ];
